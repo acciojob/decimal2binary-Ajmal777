a6fd01362843681faf7485677e39f63ed06ebc4e
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-15 19:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function threeSum(arr, target) {
-	while(target > 0){
-		arr.shift(target % 2);
-		target = (target >> 1);
+	arr.sort((a,b) => a - b);
+	const n = arr.length;
+	let ans = 0, minDiff = Number.MAX_VALUE;
+	for(let i=0; i<n-2; i++){
+		let j=i+1, k=n-1;
+		while(j < k){
+			const sum = arr[i] + arr[j] + arr[k];
+			if(sum < target){
+				j++;
+			}
+			else k--;
+			let diff = Math.abs(sum - target);
+			if(diff < minDiff){
+				minDiff = diff;
+				ans = sum;
+			}
+		}
 	}
-	return arr;
+	return sum;
 }
 
 module.exports = threeSum;
